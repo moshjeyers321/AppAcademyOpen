821443c190ba2507430b8d36143bae8002ea3e4a
Josh Meyers <pianoman04428@gmail.com>
2021-04-15 16:07:33 -0500

    add children to TTTN

--- projects/tic_tac_two/lib/tic_tac_toe_node.rb
+++ projects/tic_tac_two/lib/tic_tac_toe_node.rb
@@ -4,14 +4,28 @@ class TicTacToeNode
   attr_reader :board, :next_mover_mark, :prev_move_pos
 
   def initialize(board, next_mover_mark, prev_move_pos = nil)
-    @board, @next_mover_mark, @prev_move_pos = board, next_mover_mark, prev_move_pos
-    # @board = board
-    # @next_mover_mark = next_mover_mark
-    # @prev_move_pos = prev_move_pos
+    @board, @next_mover_mark, @prev_move_pos = 
+      board, next_mover_mark, prev_move_pos
   end
 
   def children
-    
+    children = []
+
+    (0..2).each do |row_i|
+      (0..2).each do |col_i|
+        pos = [row_i, col_i]
+
+        next unless board.empty?(pos)
+
+        new_board = board.dup
+        new_board[pos] = self.next_mover_mark
+        next_mover_mark = (self.next_mover_mark == :x ? :o : :x)
+
+        children << TicTacToeNode.new(new_board, next_mover_mark, pos)
+
+      end
+    end
+    children
   end
 
   def losing_node?(evaluator)
