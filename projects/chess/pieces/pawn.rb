e22ad5c28deeb1392becd1935f59ab92d4c38544
Josh Meyers <pianoman04428@gmail.com>
2021-04-27 16:07:08 -0500

    most pieces are finished, need to finish pawn with the side attacks

---
+++ projects/chess/pieces/pawn.rb
@@ -0,0 +1,40 @@
+require_relative 'piece'
+
+class Pawn < Piece
+
+  def symbol
+    '♟'.colorize(color)
+  end
+
+  def moves
+    forward_steps + side_attacks
+  end
+
+  private
+
+  def at_start_row?
+    pos[0] == (color == :white) ? 6 : 1
+  end
+
+  def forward_dir
+    color == :white ? -1 : 1
+  end
+
+  def forward_steps
+
+    i, j = pos
+    one_step = [i + forward_dir, j]
+    return [] unless board.valid_pos?(one_step) && board.empty?(one_step)
+
+    steps = [one_step]
+    two_step = [i + 2 * forward_dir, j]
+    steps << two_step if at_start_row? && board.empty?(two_step)
+    steps
+  end
+
+
+
+
+
+
+end
--- projects/chess/pieces/piece.rb
+++ projects/chess/pieces/piece.rb
@@ -6,6 +6,9 @@ class Piece
   attr_accessor :pos
 
   def initialize(color, board, pos)
+    raise 'invalid color' unless %i(white black).include?(color)
+    raise 'invalid pos' unless board.valid_pos?(pos)
+    
     @color, @board, @pos = color, board, pos
 
     board.add_piece(self, pos)
