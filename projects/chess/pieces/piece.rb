85a4d55c6570f219c3f295edd067b671f4dcf70c
Josh Meyers <pianoman04428@gmail.com>
2021-04-27 14:55:49 -0500

    added the null piece

--- projects/chess/pieces/piece.rb
+++ projects/chess/pieces/piece.rb
@@ -8,26 +8,34 @@ class Piece
   def initialize(color, board, pos)
     @color, @board, @pos = color, board, pos
 
+    board.add_piece(self, pos)
   end
 
   def to_s
+    " #{symbol} "
   end
 
   def empty?
+    false
   end
 
   def valid_moves
+    moves.reject { |end_pos| move_into_check?(end_pos) }
   end
 
   def pos=(val)
   end
 
   def symbol
+    raise NotImplementedError
   end
 
   private
 
   def move_into_check?(end_pos)
+    test_board = board.dup
+    test_board.move_piece!(pos, end_pos)
+    test_board.in_check?(color)
   end
 
 
