11d0d73578012a482ae2f9f7b2811ba9721accac
Josh Meyers <pianoman04428@gmail.com>
2021-05-14 17:54:03 -0500

    hash_map done

--- projects/hashmapping/lib/p05_hash_map.rb
+++ projects/hashmapping/lib/p05_hash_map.rb
@@ -1,7 +1,8 @@
 require_relative 'p04_linked_list'
 
 class HashMap
-  attr_accessor :count
+  include Enumerable
+  attr_accessor :count, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
@@ -9,27 +10,43 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    resize! if self.count >= num_buckets
+
+    if include?(key)
+      bucket(key).update(key, val)
+    else
+      bucket(key).append(key, val)
+      self.count += 1
+    end
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    self.count -= 1 if bucket(key).remove(key)
   end
 
   def each
+    self.store.each do |bucket|
+      bucket.each do |node|
+        yield [node.key, node.val]
+      end
+    end
   end
 
   # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -41,9 +58,18 @@ class HashMap
   end
 
   def resize!
+    old_store = self.store
+
+    self.count = 0
+    self.store = Array.new(num_buckets * 2) { LinkedList.new }
+
+    old_store.each do |bucket|
+      bucket.each { |link| set(link.key, link.val) }
+    end
   end
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    self.store[key.hash % num_buckets]
   end
 end
