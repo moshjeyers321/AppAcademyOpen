dff26aac4a9a6864ca6936871d61fc9631d9825f
Josh Meyers <pianoman04428@gmail.com>
2021-06-30 15:59:11 -0500

    yup

---
+++ projects/rails/URLShortener/app/models/shortened_url.rb
@@ -0,0 +1,64 @@
+# == Schema Information
+#
+# Table name: shortened_urls
+#
+#  id           :bigint           not null, primary key
+#  long_url     :string           not null
+#  short_url    :string           not null
+#  submitter_id :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+class ShortenedUrl < ApplicationRecord
+  validates :long_url, :short_url, :submitter_id, presence: true
+  validates :short_url, uniqueness: true
+
+  belongs_to :submitter,
+    primary_key: :id,
+    class_name: :User,
+    foreign_key: :submitter_id
+
+  has_many :visits,
+    primary_key: :id,
+    foreign_key: :shortened_url_id,
+    class_name: :Visit,
+    dependent: :destroy
+
+  has_many :visitors,
+    -> { distinct },
+    through: :visits,
+    source: :visitor
+
+  def self.create_for_user_and_long_url!(user, long_url)
+    ShortenedUrl.create!(
+      submitter_id: user.id,
+      long_url: long_url,
+      short_url: ShortenedUrl.random_code
+    )
+  end
+
+  def self.random_code
+    loop do
+      random_code = SecureRandom.urlsafe_base64(16)
+      return random_code unless ShortenedUrl.exists?(short_url: random_code)
+    end
+  end
+
+
+  def num_clicks
+    visits.count
+  end
+
+  def num_uniques
+    visitors.count
+  end
+
+  def num_recent_uniques
+    visits
+      .select('user_id')
+      .where('created_at > ?', 10.minutes.ago)
+      .distinct
+      .count
+  end
+
+end
