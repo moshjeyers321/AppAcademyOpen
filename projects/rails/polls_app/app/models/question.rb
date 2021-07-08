fad96a819d29483e7e87ab2233d79e4b6ba4fb22
Josh Meyers <pianoman04428@gmail.com>
2021-07-08 16:09:34 -0500

    polls seed written

---
+++ projects/rails/polls_app/app/models/question.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id         :bigint           not null, primary key
+#  text       :string           not null
+#  poll_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Question < ApplicationRecord
+  validates :text, presence: true
+
+  has_many :answer_choices,
+    primary_key: :id,
+    foreign_key: :question_id,
+    class_name: 'AnswerChoice'
+
+  belongs_to :poll,
+    primary_key: :id,
+    foreign_key: :poll_id,
+    class_name: 'Poll'
+end
---
+++ projects/rails/polls_app/app/models/response.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: responses
+#
+#  id               :bigint           not null, primary key
+#  answer_choice_id :integer          not null
+#  respondent_id    :integer          not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+class Response < ApplicationRecord
+
+  belongs_to :answer_choice,
+    primary_key: :id,
+    foreign_key: :answer_choice_id,
+    class_name: 'AnswerChoice'
+    
+    belongs_to :respondent,
+    primary_key: :id,
+    foreign_key: :respondent_id,
+    class_name: 'Respondent'
+
+
+end
