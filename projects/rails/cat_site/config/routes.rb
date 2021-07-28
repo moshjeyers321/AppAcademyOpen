a0c7a903669c47a42bd7ffcdc37b89f757617c31
Josh Meyers <pianoman04428@gmail.com>
2021-07-28 17:16:56 -0500

    CAAAAAAAAAAAATS

--- projects/rails/cat_site/config/routes.rb
+++ projects/rails/cat_site/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cats
+  # resources :cats, only: [:index, :show, :new, :create, :edit]
+  resources :cats, except: :destroy
 
 end
